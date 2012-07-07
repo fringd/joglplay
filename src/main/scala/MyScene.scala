42833345c62aaf1f1049bc8bb1e7704a31c6eb11
Ram Dobson <ram.dobson@solsystemscompany.com>
2012-07-07 19:21:44 -0400

    close to done, time to try and run it

--- src/main/scala/MyScene.scala
+++ src/main/scala/MyScene.scala
@@ -8,10 +8,163 @@ import java.nio.{IntBuffer,FloatBuffer}
 
 object MyScene extends GLEventListener {
 
+/* Introducing the OpenGL ES 2 Vertex shader
+ *
+ * The main loop inside the vertex shader gets executed
+ * one time for each vertex.
+ *
+ *      vertex -> *       uniform data -> mat4 projection = ( 1, 0, 0, 0,
+ *      (0,1,0)  / \                                          0, 1, 0, 0,
+ *              / . \  <- origo (0,0,0)                       0, 0, 1, 0,
+ *             /     \                                        0, 0,-1, 1 );
+ *  vertex -> *-------* <- vertex
+ *  (-1,-1,0)             (1,-1,0) <- attribute data can be used
+ *                        (0, 0,1)    for color, position, normals etc.
+ *
+ * The vertex shader recive input data in form of
+ * "uniform" data that are common to all vertex
+ * and
+ * "attribute" data that are individual to each vertex.
+ * One vertex can have several "attribute" data sources enabled.
+ *
+ * The vertex shader produce output used by the fragment shader.
+ * gl_Position are expected to get set to the final vertex position.
+ * You can also send additional user defined
+ * "varying" data to the fragment shader.
+ *
+ * Model Translate, Scale and Rotate are done here by matrix-multiplying a
+ * projection matrix against each vertex position.
+ *
+ * The whole vertex shader program are a String containing GLSL ES language
+ * http://www.khronos.org/registry/gles/specs/2.0/GLSL_ES_Specification_1.0.17.pdf
+ * sent to the GPU driver for compilation.
+ */
 
+  val vertexShader =
+"""#ifdef GL_ES
+#version 100
+precision mediump float;
+precision mediump int;
+#else
+#version 110
+#endif
+uniform mat4    uniform_Projection;
+attribute vec4  attribute_Position;
+attribute vec4  attribute_Color;
+varying vec4    varying_Color;
+void main(void)
+{
+  varying_Color = attribute_Color;
+  gl_Position = uniform_Projection * attribute_Position;
+}
+"""
+
+  /* Introducing the OpenGL ES 2 Fragment shader
+   *
+   * The main loop of the fragment shader gets executed for each visible
+   * pixel fragment on the render buffer.
+   *
+   *       vertex-> *
+   *      (0,1,-1) /f\
+   *              /ffF\ <- This fragment F gl_FragCoord get interpolated
+   *             /fffff\                   to (0.25,0.25,-1) based on the
+   *   vertex-> *fffffff* <-vertex         three vertex gl_Position.
+   *  (-1,-1,-1)           (1,-1,-1)
+   *
+   *
+   * All incomming "varying" and gl_FragCoord data to the fragment shader
+   * gets interpolated based on the vertex positions.
+   *
+   * The fragment shader produce and store the final color data output into
+   * gl_FragColor.
+   *
+   * Is up to you to set the final colors and calculate lightning here based on
+   * supplied position, color and normal data.
+   *
+   * The whole fragment shader program are a String containing GLSL ES language
+   * http://www.khronos.org/registry/gles/specs/2.0/GLSL_ES_Specification_1.0.17.pdf
+   * sent to the GPU driver for compilation.
+   */
+  val fragmentShader =
+"""#ifdef GL_ES
+#version 100
+precision mediump float;
+precision mediump int;
+#else
+#version 110
+#endif
+
+varying   vec4    varying_Color;
+void main (void)
+{
+  gl_FragColor = varying_Color;
+}
+"""
+  /* Introducing projection matrix helper functions
+   *
+   * OpenGL ES 2 vertex projection transformations gets applied inside the
+   * vertex shader, all you have to do are to calculate and supply a projection matrix.
+   *
+   * Its recomended to use the com/jogamp/opengl/util/PMVMatrix.java
+   * import com.jogamp.opengl.util.PMVMatrix;
+   * To simplify all your projection model view matrix creation needs.
+   *
+   * These helpers here are based on PMVMatrix code and common linear
+   * algebra for matrix multiplication, translate and rotations.
+   */
+  def glMultMatrixf(a:FloatBuffer, b:FloatBuffer, d:FloatBuffer) = {
+    val aP = a.position()
+    val bP = b.position()
+    val dP = d.position()
+    for (int i = 0; i < 4; i++) {
+      val (ai0, ai1, ai2, ai3) = (a.get(aP+i+0*4),  ai1=a.get(aP+i+1*4),  ai2=a.get(aP+i+2*4),  ai3=a.get(aP+i+3*4)
+      d.put(dP+i+0*4 , ai0 * b.get(bP+0+0*4) + ai1 * b.get(bP+1+0*4) + ai2 * b.get(bP+2+0*4) + ai3 * b.get(bP+3+0*4) )
+      d.put(dP+i+1*4 , ai0 * b.get(bP+0+1*4) + ai1 * b.get(bP+1+1*4) + ai2 * b.get(bP+2+1*4) + ai3 * b.get(bP+3+1*4) )
+      d.put(dP+i+2*4 , ai0 * b.get(bP+0+2*4) + ai1 * b.get(bP+1+2*4) + ai2 * b.get(bP+2+2*4) + ai3 * b.get(bP+3+2*4) )
+      d.put(dP+i+3*4 , ai0 * b.get(bP+0+3*4) + ai1 * b.get(bP+1+3*4) + ai2 * b.get(bP+2+3*4) + ai3 * b.get(bP+3+3*4) )
+    }
+    ()
+  }
+
+  def multiply(a:Array[Float],b:Array[Float]) = {
+    val tmp = new Array[Float](16)
+    glMultMatrixf(FloatBuffer.wrap(a),FloatBuffer.wrap(b),FloatBuffer.wrap(tmp))
+    tmp
+  }
+
+  def translate(m:Array[Float], x:Float, y:Float, z:Float) = {
+    val t = Array( 1.0f, 0.0f, 0.0f, 0.0f,
+                  0.0f, 1.0f, 0.0f, 0.0f,
+                  0.0f, 0.0f, 1.0f, 0.0f,
+                  x, y, z, 1.0f )
+    return multiply(m, t)
+  }
+
+  def rotate(m:Array[Float],a:Float, x:Float, y:Float, z:Float) = {
+    val (s,c) = (0.0f, 0.0f)
+    s = (float)Math.sin(Math.toRadians(a))
+    c = (float)Math.cos(Math.toRadians(a))
+    val r = Array(
+      x * x * (1.0f - c) + c,     y * x * (1.0f - c) + z * s, x * z * (1.0f - c) - y * s, 0.0f,
+      x * y * (1.0f - c) - z * s, y * y * (1.0f - c) + c,     y * z * (1.0f - c) + x * s, 0.0f,
+      x * z * (1.0f - c) + y * s, y * z * (1.0f - c) - x * s, z * z * (1.0f - c) + c,     0.0f,
+      0.0f, 0.0f, 0.0f, 1.0f
+    )
+    return multiply(m, r)
+  }
+
+  def printMatrix( m:Array[Float] ):Unit = {
+    print( ("%f %f %f %f\n"*4).format(m:_*) )
+  }
+
+
+  /* variables */
   var (shaderProgram, vertShader fragShader) = (0,0,0)
 
   var ModelViewProjectionMatrix_location = 0
+
+  var (theta, s, c) = (0.0, 0.0, 0.0)
+
 
   def display(drawable:GLAutoDrawable) = {
     update()
@@ -93,19 +246,98 @@ object MyScene extends GLEventListener {
   }
 
   def dispose(drawable:GLAutoDrawable) = {
+    System.out.println("cleanup, remember to release shaders")
+    GL2ES2 gl = drawable.getGL().getGL2ES2()
+    gl.glUseProgram(0)
+    gl.glDetachShader(shaderProgram, vertShader)
+    gl.glDeleteShader(vertShader)
+    gl.glDetachShader(shaderProgram, fragShader)
+    gl.glDeleteShader(fragShader)
+    gl.glDeleteProgram(shaderProgram)
+    System.exit(0)
   }
 
   def reshape(drawable:GLAutoDrawable, x:Int, y:Int, width:Int, height:Int) = {
   }
 
   def update() = {
-    x = x - 0.002.toFloat
+    theta += 0.08
+    s = Math.sin(theta)
+    c = Math.cos(theta)
     ()
   }
 
   def render(drawable:GLAutoDrawable) = {
     val gl = drawable.getGL().getGL2ES2()
-    gl.glClear(GL.GL_COLOR_BUFFER_BIT)
+    gl.glClearColor(1,0,1,1) //purple
+    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+
+    gl.glUseProgram(shaderProgram);
+
+
+    /* Change a projection matrix
+     * The matrix multiplications and OpenGL ES2 code below
+     * basically match this OpenGL ES1 code.
+     * note that the model_view_projection matrix gets sent to the vertexShader.
+     *
+     * gl.glLoadIdentity();
+     * gl.glTranslatef(0.0f,0.0f,-0.1f);
+     * gl.glRotatef((float)30f*(float)s,1.0f,0.0f,1.0f);
+     *
+     */
+
+    var model_view_projection = new Array[Float](0)
+    val identity_matrix = Array(
+      1.0f, 0.0f, 0.0f, 0.0f,
+      0.0f, 1.0f, 0.0f, 0.0f,
+      0.0f, 0.0f, 1.0f, 0.0f,
+      0.0f, 0.0f, 0.0f, 1.0f,
+    )
+
+    model_view_projection =  translate(identity_matrix,0.0f,0.0f, -0.1f)
+    model_view_projection =  rotate(model_view_projection,30f*s,1.0f,0.0f,1.0f)
+
+    // Send the final projection matrix to the vertex shader by
+    // using the uniform location id obtained during the init part.
+    gl.glUniformMatrix4fv(ModelViewProjectionMatrix_location, 1, false, model_view_projection, 0)
+
+    /*
+     *  Render a triangle:
+     *  The OpenGL ES2 code below basically match this OpenGL code.
+     *
+     *    gl.glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
+     *    gl.glVertex3f( 0.0f, 1.0f, 0.0f);              // Top
+     *    gl.glVertex3f(-1.0f,-1.0f, 0.0f);              // Bottom Left
+     *    gl.glVertex3f( 1.0f,-1.0f, 0.0f);              // Bottom Right
+     *    gl.glEnd();                            // Finished Drawing The Triangle
+     */
+
+    val vertices = Array(
+      0.0f,  1.0f, 0.0f, //Top
+      -1.0f, -1.0f, 0.0f, //Bottom Left
+      1.0f, -1.0f, 0.0f  //Bottom Right
+    )
+
+    gl.glVertexAttribPointer(0, 3, GL.GL_FLOAT, false, 0, FloatBuffer.wrap(vertices))
+    gl.glEnableVertexAttribArray(0)
+
+    val colors = Array(
+      1.0f, 0.0f, 0.0f, //Top color (red)
+      0.0f, 0.0f, 0.0f, //Bottom Left color (black)
+      1.0f, 1.0f, 0.0f  //Bottom Right color (yellow)
+    )
+
+
+    gl.glVertexAttribPointer(1, 3, GL.GL_FLOAT, false, 0, FloatBuffer.wrap(colors))
+    gl.glEnableVertexAttribArray(1)
+
+    gl.glDrawArrays(GL2ES2.GL_TRIANGLES, 0, 3); //Draw the vertices as triangle
+    
+    gl.glDisableVertexAttribArray(0); // Allow release of vertex position memory
+    gl.glDisableVertexAttribArray(1); // Allow release of vertex color memory		
+
+
+
 
     gl.glEnableVertexAttribArray(0)
     gl.glBindBuffer(GL.GL_ARRAY_BUFFER, indices(0))
@@ -117,30 +349,4 @@ object MyScene extends GLEventListener {
     drawable.swapBuffers()
   }
 
-  def loadShaders(gl:GL2ES2) = {
-    val v = gl.glCreateShader(GL2ES2.GL_VERTEX_SHADER)
-    val f = gl.glCreateShader(GL2ES2.GL_FRAGMENT_SHADER)
-
-    var source = scala.io.Source.fromFile("glsl/vertex.glsl")
-    val vsrc = source.getLines.mkString("\n")
-    source.close()
-
-    gl.glShaderSource(v, 1, Array(vsrc), null)
-    gl.glCompileShader(v)
-
-    source = scala.io.Source.fromFile("glsl/fragment.glsl")
-    val fsrc = source.getLines.mkString("\n")
-    source.close()
-
-    gl.glShaderSource(f, 1, Array(fsrc), null)
-    gl.glCompileShader(f)
-
-    val shaderprogram = gl.glCreateProgram()
-    gl.glAttachShader(shaderprogram, v)
-    gl.glAttachShader(shaderprogram, f)
-    gl.glLinkProgram(shaderprogram)
-    gl.glValidateProgram(shaderprogram)
-
-    gl.glUseProgram(shaderprogram)
-  }
 }
